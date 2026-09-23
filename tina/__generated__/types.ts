8e96556abb6dde98bdc5998a7b199eedb1cad337
3D Creations <info@3dcreationspa.com>
2026-09-22 21:34:12 -0400

    chore: generated tina schema

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -682,7 +682,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/3.0/content/d83d34dc-2ed4-4e39-a2c5-7a85523b655f/github/main",
         queries,
       })
     )
